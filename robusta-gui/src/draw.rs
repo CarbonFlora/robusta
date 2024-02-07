e47d5ab9356e58a6ddfb1d036205f9e1b54fcc1e
CarbonFlora <zihaoliang0413@gmail.com>
2024-02-06 16:43:11 -0800

    simple mesh

--- robusta-gui/src/draw.rs
+++ robusta-gui/src/draw.rs
@@ -1,4 +1,7 @@
-use bevy::sprite::MaterialMesh2dBundle;
+use bevy::{
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+    sprite::MaterialMesh2dBundle,
+};
 use bevy_mod_picking::{prelude::*, PickableBundle};
 
 use crate::*;
@@ -49,23 +52,62 @@ fn draw_lines(
     wrapper: &DXFWrapper,
 ) {
     for line in &wrapper.lines {
-        // Create my own mesh for lines.
-        // commands.spawn((
-        //     MaterialMesh2dBundle {
-        //         mesh: meshes
-        //             .add(shape::Quad::new(Vec2 { x: 5., y: 15. }).into())
-        //             .into(),
-        //         material: materials.add(ColorMaterial::from(Color::WHITE)),
-        //         transform: Transform::from_translation(Vec3::new(
-        //             line.definition[0].coordinates.x,
-        //             line.definition[0].coordinates.y,
-        //             0.,
-        //         )),
-        //         ..default()
-        //     },
-        //     PickableBundle::default(),
-        //     On::<Pointer<Select>>::send_event::<SelectionInstance>(),
-        //     On::<Pointer<Deselect>>::send_event::<SelectionInstance>(),
-        // ));
+        commands.spawn((
+            MaterialMesh2dBundle {
+                // mesh: meshes
+                //     .add(shape::Quad::new(Vec2 { x: 5., y: 15. }).into())
+                //     .into(),
+                mesh: meshes.add(create_simple_parallelogram().into()).into(),
+                material: materials.add(ColorMaterial::from(Color::WHITE)),
+                transform: Transform::from_translation(Vec3::new(
+                    line.definition[0].coordinates.x,
+                    line.definition[0].coordinates.y,
+                    0.,
+                )),
+                ..default()
+            },
+            PickableBundle::default(),
+            On::<Pointer<Select>>::send_event::<SelectionInstance>(),
+            On::<Pointer<Deselect>>::send_event::<SelectionInstance>(),
+        ));
     }
+}
+
+fn create_simple_parallelogram() -> Mesh {
+    // Create a new mesh using a triangle list topology, where each set of 3 vertices composes a triangle.
+    // https://github.com/bevyengine/bevy/blob/main/assets/docs/Mesh.png
+    Mesh::new(PrimitiveTopology::TriangleList)
+        // Add 4 vertices, each with its own position attribute (coordinate in
+        // 3D space), for each of the corners of the parallelogram.
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            vec![
+                [0.0, 0.0, 0.0],
+                [1.0, 2.0, 0.0],
+                [2.0, 2.0, 0.0],
+                [1.0, 0.0, 0.0],
+            ],
+        )
+        // Assign a UV coordinate to each vertex.
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_UV_0,
+            vec![[0.0, 1.0], [0.5, 0.0], [1.0, 0.0], [0.5, 1.0]],
+        )
+        // Assign normals (everything points outwards)
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            vec![
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
+            ],
+        )
+        // After defining all the vertices and their attributes, build each triangle using the
+        // indices of the vertices that make it up in a counter-clockwise order.
+        .with_indices(Some(Indices::U32(vec![
+            // First triangle
+            0, 3, 1, // Second triangle
+            1, 3, 2,
+        ])))
 }
